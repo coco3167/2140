375e4502378b2398f80afd06edc47fe42a3588c7
Corentin <corentinboblet@gmail.com>
2025-07-11 14:57:00 +0200

    Added restart button

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,19 @@
+using System;
+using UnityEngine;
+using UnityEngine.InputSystem;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    [SerializeField] private InputActionReference restart;
+
+    private void Awake()
+    {
+        restart.ToInputAction().started += Restart;
+    }
+
+    private void Restart(InputAction.CallbackContext context)
+    {
+        SceneManager.LoadScene(0);
+    }
+}
