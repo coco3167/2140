de856f79469a31b66200c74e28379150df753daa
Corentin <corentinboblet@gmail.com>
2025-07-11 11:17:00 +0200

    Camera rotation by mouse

---
+++ Assets/Scripts/CameraRotation.cs
@@ -0,0 +1,42 @@
+using UnityEngine;
+using UnityEngine.InputSystem;
+
+public class CameraRotation : MonoBehaviour
+{
+    [SerializeField] private Transform mainCamera;
+    [SerializeField] private InputActionReference look;
+
+    [SerializeField, Range(0.1f, 1)] private float rotationSpeed;
+
+    private float m_pitch, m_yaw;
+
+    private void Start()
+    {
+        Cursor.lockState = CursorLockMode.Locked;
+        Cursor.visible = false;
+    }
+
+    void Update()
+    {
+        Vector2 lookingVector = look.ToInputAction().ReadValue<Vector2>();
+        
+        m_pitch += rotationSpeed * -lookingVector.y;
+        m_yaw += rotationSpeed * lookingVector.x;
+
+        // Clamp pitch:
+        m_pitch = Mathf.Clamp(m_pitch, -90f, 90f);
+
+        // Wrap yaw:
+        while (m_yaw < 0f)
+        {
+            m_yaw += 360f;
+        }
+
+        while (m_yaw >= 360f)
+        {
+            m_yaw -= 360f;
+        }
+
+        mainCamera.eulerAngles = new Vector3(m_pitch, m_yaw, 0);
+    }
+}
