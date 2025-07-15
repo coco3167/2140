4fd4843af3aa052189f32c30c5ef07eedd95e1cb
Corentin <corentinboblet@gmail.com>
2025-07-15 14:57:57 +0200

    Rotating square when hover on psymanite

--- Assets/Scripts/CrystalDetector.cs
+++ Assets/Scripts/CrystalDetector.cs
@@ -1,3 +1,4 @@
+using DG.Tweening;
 using UnityEngine;
 using UnityEngine.UI;
 
@@ -9,9 +10,12 @@ public class CrystalDetector : MonoBehaviour
     [Header("Cursor")]
     [SerializeField] private Image cursorImage;
     [SerializeField] private Color baseColor, lookingColor;
+    [SerializeField] private float transitionTime;
     
     private Ray m_detectingRay;
     private RaycastHit[] m_hitsBuffer = new RaycastHit[5];
+
+    private Sequence m_lookedAtSequence;
 
     private Camera m_camera;
 
@@ -20,10 +24,17 @@ public class CrystalDetector : MonoBehaviour
     private void Awake()
     {
         m_camera = GetComponent<Camera>();
+
+        m_lookedAtSequence = DOTween.Sequence();
+        m_lookedAtSequence.Append(cursorImage.rectTransform.DOLocalRotate(new Vector3(0, 0, 45), transitionTime));
+        m_lookedAtSequence.Join(cursorImage.DOColor(lookingColor, transitionTime));
+        m_lookedAtSequence.SetAutoKill(false);
     }
 
     private void FixedUpdate()
     {
+        bool hasTouchedPsymanite = false;
+        
         m_detectingRay = m_camera.ViewportPointToRay(CameraCenter);
 
         int hits = Physics.RaycastNonAlloc(m_detectingRay, m_hitsBuffer, Mathf.Infinity, psymaniteLayerMask,
@@ -31,11 +42,21 @@ public class CrystalDetector : MonoBehaviour
 
         for (int loop = 0; loop < hits; loop++)
         {
-            if(m_hitsBuffer[loop].transform.TryGetComponent(out Psymanite psymanite))
+            if (m_hitsBuffer[loop].transform.TryGetComponent(out Psymanite psymanite))
+            {
+                hasTouchedPsymanite = true;
                 psymanite.OnLookedAt();
+            }
         }
 
-        cursorImage.color = hits > 0 ? lookingColor : baseColor;
+        if (hasTouchedPsymanite)
+        {
+            m_lookedAtSequence.PlayForward();
+        }
+        else
+        {
+            m_lookedAtSequence.PlayBackwards();
+        }
     }
 
     private void OnDrawGizmos()
